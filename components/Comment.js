dc4394572625b62b76ac7614d49231ea85a40b8e
coderblip02 <105997900+coderblip02@users.noreply.github.com>
2022-07-02 21:37:15 -0400

    Get The Comments from firestore and create the comments component

---
+++ components/Comment.js
@@ -0,0 +1,8 @@
+
+function Comment({comment, id}) {
+  return (
+    <div>{comment.comments}</div>
+  )
+}
+
+export default Comment
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,18 +4,40 @@ import Widgets from "../../components/Widgets";
 import CommentModal from "../../components/CommentModal";
 import Post from "../../components/Post";
 import { ArrowLeftIcon } from "@heroicons/react/outline";
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-import { doc, onSnapshot } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import {
+  doc,
+  onSnapshot,
+  query,
+  collection,
+  orderBy,
+} from "firebase/firestore";
 import { db } from "../../firebase";
+import Comment from "../../components/comment";
 
 export default function PostPage({ newsResults, randomUsersResults }) {
-    const router = useRouter();
-    const { id } = router.query;
-    const [post, setPost] = useState();
+  const router = useRouter();
+  const { id } = router.query;
+  const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
 
+  useEffect(
+    () => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)),
+    [db, id]
 
-    useEffect(() => onSnapshot(doc(db, "posts", id), (snapshot)=> setPost(snapshot)), [db, id])
+    // get comments follow
+  );
+
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => setComments(snapshot.docs)
+    );
+  }, [db, id]);
   return (
     <div>
       <Head>
@@ -33,17 +55,26 @@ export default function PostPage({ newsResults, randomUsersResults }) {
 
         <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
           <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
-            <div onClick={() => router.push("/")}className="hoverEffect ">
-              <ArrowLeftIcon className="h-5"/>
+            <div onClick={() => router.push("/")} className="hoverEffect ">
+              <ArrowLeftIcon className="h-5" />
             </div>
             <h2 className="text-lg sm:text-xl font-bold cursor-pointer">
               Tweet
             </h2>
           </div>
-          <Post id={id} post={post}/>
+          <Post id={id} post={post} />
+          {comments.length > 0 && (
+            <div className="">
+              {comments.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  id={comment.id}
+                  comment={comment.data()}
+                />
+              ))}
+            </div>
+          )}
         </div>
-
-        {/* widgets */}
 
         <Widgets
           newsResults={newsResults.articles}
