f463b3292f9cdb2f40e5d792f91d9c26065c5258
coderblip02 <105997900+coderblip02@users.noreply.github.com>
2022-07-04 08:08:53 -0400

    fixing arrow issue in post page

--- components/Feed.js
+++ components/Feed.js
@@ -1,15 +1,13 @@
 import { SparklesIcon } from "@heroicons/react/outline";
-import React from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Input from "./Input";
 import Post from "./Post";
-import { useState, useEffect } from "react";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { db } from "../firebase";
-import { AnimatePresence, motion } from "framer-motion";
 
-function Feed() {
+export default function Feed() {
   const [posts, setPosts] = useState([]);
-
   useEffect(
     () =>
       onSnapshot(
@@ -20,12 +18,11 @@ function Feed() {
       ),
     []
   );
-
   return (
-    <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+    <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
-        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9 ">
+        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
           <SparklesIcon className="h-5" />
         </div>
       </div>
@@ -46,5 +43,3 @@ function Feed() {
     </div>
   );
 }
-
-export default Feed;
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -54,8 +54,8 @@ export default function PostPage({ newsResults, randomUsersResults }) {
         {/* feed */}
 
         <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
-          <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
-            <div onClick={() => router.push("/")} className="hoverEffect ">
+          <div className="flex items-center space-x-3 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+            <div onClick={() => router.push("/")}>
               <ArrowLeftIcon className="h-5" />
             </div>
             <h2 className="text-lg sm:text-xl font-bold cursor-pointer">
